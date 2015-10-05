08995f66556a07a7ddefdec55afcd99d74c24650
Marc-André Tremblay <marcandre.tr@gmail.com>
2015-10-05 16:10:01 -0400

    Faster sum it up
    N: O(N)
    Q: O(1)

--- sum_it_up/sum_it_up.py
+++ sum_it_up/sum_it_up.py
@@ -3,30 +3,7 @@ __author__ = 'marc'
 import operator
 import functools
 
-def backcycler(seq, back_index):
-    seq_len = len(seq)
-    idx = seq_len - (back_index % seq_len)
-
-    while True:
-        idx %= seq_len
-        yield seq[idx]
-        idx += 1
-
-
 input() # we dont care much for this one
-
-seq = [int(x) for x in input().split()]
-
+seq = map(int, input().split())
 num_of_operations = int(input())
-
-for i in range(num_of_operations):
-    new_seq = seq[:]
-
-    backcycle = backcycler(seq, int(input()))
-
-    for j in range(len(seq)):
-        new_seq[j] += next(backcycle)
-
-    seq = new_seq
-
-print(functools.reduce(operator.add, seq) % (10**9 + 7))
+print(functools.reduce(operator.add, seq) * 2 ** num_of_operations % (10**9 + 7))
